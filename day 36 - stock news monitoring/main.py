8aca21a084dacf7a94bb0b4164efb66c1ed10b68
FlavioRyu <flavioryu@gmail.com>
2026-06-16 00:52:45 +0100

    Finish day 36 project

--- day 36 - stock news monitoring/main.py
+++ day 36 - stock news monitoring/main.py
@@ -3,18 +3,25 @@ import requests_cache
 import os
 
 
-# create requests caches
-requests_cache.install_cache('stocks_cache')
-requests_cache.install_cache('news_cache')
+# create requests caches for testing
+# requests_cache.install_cache('stocks_cache')
+# requests_cache.install_cache('news_cache')
 
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHAVANTAGE_API_URL = "https://www.alphavantage.co/query"
-NEWSAPI_API_URL = "https://newsapi.org"
+NEWSAPI_API_URL = "https://newsapi.org/v2/everything"
+TEXTBEE_API_URL = "https://api.textbee.dev/api/v1"
 # to add env variables in windows ps, run this in pycharm terminal or elevated ps and restart pycharm
 # [System.Environment]::SetEnvironmentVariable("VARIABLE", "VALUE", "User")
 
+
+def api_get(api_url: str, parameters: dict) -> str:
+    rsp = requests.get(api_url, params=parameters)
+    rsp.raise_for_status()
+    print(f"Source: {'CACHE' if getattr(rsp, 'from_cache', False) else 'API'}")
+    return rsp.json()
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -24,41 +31,51 @@ stock_params = {
     "apikey": os.getenv("ALPHAVANTAGE_API_KEY"),
     "datatype": "json"
 }
-stock_response = requests.get(ALPHAVANTAGE_API_URL, params=stock_params)
-stock_response.raise_for_status()
-print(f"Source: {'CACHE' if getattr(stock_response, 'from_cache', False) else 'API'}")
-daily_results = dict(stock_response.json()["Time Series (Daily)"])
-# print(daily_results)
+daily_results_json = api_get(ALPHAVANTAGE_API_URL, stock_params)
+daily_results = dict(daily_results_json["Time Series (Daily)"])
 gestern = sorted(daily_results.keys())[-2]
 gestern_closing = float(daily_results[gestern]["4. close"])
-vorgestern = sorted(daily_results.keys())[-34]
+vorgestern = sorted(daily_results.keys())[-3]
 vorgestern_closing = float(daily_results[vorgestern]["4. close"])
-print(vorgestern_closing, gestern_closing)
 diff_percentage = (gestern_closing - vorgestern_closing) / gestern_closing * 100
-print(diff_percentage)
-if abs(diff_percentage) > 5:
-    print("Get News")
 
+# debug
+print(vorgestern, gestern)
+print(vorgestern_closing, gestern_closing)
+print(diff_percentage)
 
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+if abs(diff_percentage) > 1:
+    news_params = {
+        "apiKey": os.getenv("NEWSAPI_API_KEY"),
+        "q": COMPANY_NAME,
+        "sortBy": "publishedAt",
+        "to": gestern,
+        "language": "en",
+    }
+    news_results_json = api_get(NEWSAPI_API_URL, news_params)
+    news_results = news_results_json["articles"]
+    top3_articles = news_results[:15]
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+    ## STEP 3: Use https://www.twilio.com
+    # Send a seperate message with the percentage change and each article's title and description to your phone number.
+    msg = "[STOCK ALERT]\n\n"
+    msg += (f"{STOCK}: {'🔺' if diff_percentage > 0 else '🔻'}{int(diff_percentage)}%\n\n"
+            f"Latest news regarding {COMPANY_NAME}\n\n")
+    for article in top3_articles:
+        date = article["publishedAt"].split("T")[0]
+        title = article["title"]
+        brief = article["description"]
+        url = article["url"]
+        msg += (f"Dated: {date}\n"
+                f"📢 {title}\n"
+                f"📰 {brief}\n"
+                f"Read more: {url}\n\n")
 
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file 
-by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
-coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file
-by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
-coronavirus market crash.
-"""
-
+    text_rsp = requests.post(
+        url=f'{TEXTBEE_API_URL}/gateway/devices/{os.getenv("TEXTBEE_PHONE_ID")}/send-sms',
+        json={'recipients': [os.getenv("PHONE_NUMBER")], 'message': msg},
+        headers={'x-api-key': os.getenv("TEXTBEE_API_KEY")}
+    )
+    text_rsp.raise_for_status()
