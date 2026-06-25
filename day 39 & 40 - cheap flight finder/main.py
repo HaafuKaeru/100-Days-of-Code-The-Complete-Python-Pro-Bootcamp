f1ae9fea8dce983887b9c4135f539ed820b71b0f
FlavioRyu <flavioryu@gmail.com>
2026-06-25 23:19:24 +0100

    Continue day 39 & 40 project

--- day 39 & 40 - cheap flight finder/main.py
+++ day 39 & 40 - cheap flight finder/main.py
@@ -1,6 +1,7 @@
 #This file will need to use the DataManager, FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 def main():
+    print("test")
     pass
 
 if __name__ == '__main__':
