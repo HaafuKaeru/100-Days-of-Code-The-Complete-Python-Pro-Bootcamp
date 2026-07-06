8897032bcf8a6cbf59633cae962bdc22ac7712fc
FlavioRyu <flavioryu@gmail.com>
2026-07-06 17:59:59 +0100

    Much simpler solution with 'soup.find_all'

--- day 45 - 100 movies to watch/main.py
+++ day 45 - 100 movies to watch/main.py
@@ -7,20 +7,20 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, "html.parser")
-# print(soup.prettify())
 
-css_selectors = [
-    "div.gallery",
-    "section.gallery__content-item.gallery__content-item--gallery",
-    "div.article-title-description",
-    "div.article-title-description__text",
-    "h3",
-]
-movies = [header.text for header in soup.select(" ".join(css_selectors))]
-movies = list(reversed(movies))
-# print(movies)
+# css_selectors = [
+#     "div.gallery",
+#     "section.gallery__content-item.gallery__content-item--gallery",
+#     "div.article-title-description",
+#     "div.article-title-description__text",
+#     "h3",
+# ]
+# movies = [header.text for header in soup.select(" ".join(css_selectors))]
+movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.text for movie in movies]
+movie_titles = movie_titles[::-1]
 
-movies_string = "\n".join(movies)
+movies_string = "\n".join(movie_titles)
 with open("movies.txt", "w", encoding='utf-8') as f:
     f.writelines(movies_string)
 
