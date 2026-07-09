a34a9bb4d0dc7358dd3d436e238d3882f1bf3690
FlavioRyu <flavioryu@gmail.com>
2026-07-09 02:26:42 +0100

    Finish day 46 project

--- day 46 - musical time machine/main.py
+++ day 46 - musical time machine/main.py
@@ -1,41 +1,61 @@
-import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 from dotenv import load_dotenv
+from pprint import pprint
+import requests
 import os
+
+from spotipy_manager import SpotipyManager
 
 
 load_dotenv(".env")
-BASE_URL = "https://appbrewery.github.io/bakeboard-hot-100/"
+BILLBOARD_MOCK_URL = "https://appbrewery.github.io/bakeboard-hot-100/"
+BILLBOARD_REAL_URL = "https://www.billboard.com/charts/hot-100/"
 
 
 def main():
 
-    # user_input = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format: ")
-    user_input = "2014-10-18"
-
-    headers = {
-        "USER-AGENT": os.getenv("USER-AGENT")
-    }
+    # date = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format: ")
+    date = "2014-10-18"
 
     rsp = requests.get(
-        url=f"{BASE_URL}{user_input}/",
-        headers=headers,
+        url=f"{BILLBOARD_MOCK_URL}{date}/",
+        headers={"USER-AGENT": os.getenv("USER_AGENT")},
     )
     rsp.raise_for_status()
 
     soup = BeautifulSoup(rsp.text, "html.parser")
     entries = soup.find_all(name="h3", class_="chart-entry__title")
     top_100_songs = [tag.text for tag in entries]
-    # print(top_100_songs)
+    # pprint(top_100_songs)
 
-    # user_id = sp.current_user()["id"]
-    # print(user_id)
-    # sp.current_user_playlist_create(name="Test", description="this was created by Python")
-    # pprint(sp.current_user_playlists())
-    # sp.current_user_unfollow_playlist(playlist_id="xxx")
-    my_playlists = {playlist["name"]: playlist["id"] for playlist in sp.current_user_playlists()["items"]}
-    pprint(my_playlists)
+    # search the top 100 songs in spotify
+    sm = SpotipyManager()
+    track_uris = []
+    for song in top_100_songs:
+        # searching just by track name and year might lead to wrong results
+        search_query = {
+            "track": song,
+            "year": date.split("-")[0],
+        }
+        result = sm.search(search_query, limit=1, market="GB")
+        track_name = result["tracks"]["items"][0]["name"]
+        artist = result["tracks"]["items"][0]["artists"][0]["name"]
+        track_uri = result["tracks"]["items"][0]["uri"]
+        track_uris.append(track_uri)
+        info = (track_name, artist, track_uri)
+        # print(info)
+
+    # create new public playlist
+    new_playlist = sm.create_playlist(
+        name=f"{date} Billboard 100",
+        description="This was made by Python!"
+    )
+
+    # add all 100 tracks into it
+    sm.add_tracks_to_playlist(new_playlist, track_uris)
+
+    # pprint(sm.get_my_playlists_info(show=["items"]))
+    # sm.delete_playlist("4JE0CFWaqRbcFo8QOuHTGY")
 
 
 if __name__ == '__main__':
--- day 46 - musical time machine/spotipy_manager.py
+++ day 46 - musical time machine/spotipy_manager.py
@@ -15,7 +15,9 @@ class SpotipyManager:
         self.spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
         self.oauth = None
         self.session = None
+        self.playlists = None
         self._initialise()
+        self._load_playlists()
 
     def _create_oauth(self):
         self.oauth = SpotifyOAuth(
@@ -33,3 +35,53 @@ class SpotipyManager:
     def _initialise(self):
         self._create_oauth()
         self.session = spotipy.Spotify(oauth_manager=self.oauth)
+
+    def _load_playlists(self):
+        self.playlists = self.session.current_user_playlists()
+
+    def get_my_playlists_info(self, show=None) -> dict:
+        if not show:
+            show = [
+                "id",
+                "description",
+                "external_urls",
+                "items",
+            ]
+        my_playlists = {
+            playlist["name"]: {
+                info: playlist[info] for info in show
+            } for playlist in self.playlists["items"]
+        }
+        return my_playlists
+
+    def create_playlist(self, **kwargs) -> str:
+        output = self.session.current_user_playlist_create(**kwargs)
+        _id = output["id"]
+        print(f"\nPlaylist created: {kwargs}\nid: {_id}\n")
+        self._load_playlists()
+        return _id
+
+    def delete_playlist(self, _id: str):
+        confirmation = input(
+            f"The playlist with id: {_id} is about to be permanently deleted.\n"
+            f"Are you sure? (yes/no): "
+        )
+        if confirmation.lower() == "yes":
+            self.session.current_user_unfollow_playlist(playlist_id=_id)
+            self._load_playlists()
+
+    def get_user_id(self) -> str:
+        return self.session.current_user()["id"]
+
+    def search(self, query, **kwargs) -> dict:
+        query_str = ""
+        for key, val in query.items():
+            query_str += f"{key}:"
+            query_str += val.replace(" ", "+")
+            query_str += " "
+        # print(query_str)
+        result = self.session.search(q=query_str, **kwargs)
+        return result
+
+    def add_tracks_to_playlist(self, playlist_id: str, tracks: list):
+        self.session.playlist_add_items(playlist_id, tracks)
